37352764f35457413f6acbff635f45096fca0e69
Ikhwan S. H <ikhwansyatricha@gmail.com>
2022-03-10 09:40:23 +0700

    Modify rotate_and_resize
    Modify rotate_and_resize according to qwiklabs requirements

--- Manipulating Images/rotate_and_resize.py
+++ Manipulating Images/rotate_and_resize.py
@@ -4,12 +4,12 @@ import os
 
 from PIL import Image
 
-original_file_dir  = 'originalFiles/'
-processed_file_dir = 'processedFiles/'
+original_file_dir  = os.path.join(os.getcwd(), 'images/')
+processed_file_dir = "/opt/icons/"
 
-image_rotation = -90
-new_width  = 600
-new_height = 600
+image_rotation = -90 # 90 degree clockwise
+new_width  = 128
+new_height = 128
 
 
 def rotate_and_resize_image(filepath, newpath):
@@ -27,12 +27,12 @@ def rotate_and_resize_image(filepath, newpath):
             ).save (
                 newpath
             )
-        print ('  {} resized to {}'.format (filepath, newpath))
+        print ('  SUCCESFULLY rotated and resized{}'.format (os.path.split(filepath)[-1]))
     except:
-        print ('FAILED to resize {}'.format (filepath))
+        print ('FAILED to resize {}'.format (os.path.split(filepath)[-1]))
 
 
-def process_images(ori_dir , new_dir):
+def process_images(ori_dir, new_dir, old_format="tiff", new_format="jpeg"):
     """List all image files on the ori_dir and then save
     a new rotated and resized version of it to new_dir"""
     images = os.listdir(ori_dir)
@@ -44,13 +44,13 @@ def process_images(ori_dir , new_dir):
         
         rotate_and_resize_image (
                 os.path.join(ori_dir, image_name),
-                os.path.join(new_dir, image_name) 
+                os.path.join(new_dir, image_name[:-len(old_format)]+new_format) 
             )
 
 
 def main ():
-    ori_dir = os.path.join(os.getcwd(), original_file_dir)
-    new_dir = os.path.join(os.getcwd(), processed_file_dir)
+    ori_dir = original_file_dir
+    new_dir = processed_file_dir
     
     process_images (ori_dir, new_dir)
 
